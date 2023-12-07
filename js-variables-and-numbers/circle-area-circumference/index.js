abbc0446d068c24b1e736316c7a1b0de2926fc4f
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-07 18:34:03 +0100

    circle circumference challenge

---
+++ js-variables-and-numbers/circle-area-circumference/index.js
@@ -0,0 +1,10 @@
+let radius = 5;
+
+let circleArea = Math.PI * Math.pow(radius, 2);
+
+let circumference = 2 * Math.PI * radius;
+
+console.log("For a circle with a radius of", radius, "units:");
+
+console.log("-   The area is approximately", circleArea, "square units.");
+console.log("-   The circumference is approximately", circumference, "units.");
