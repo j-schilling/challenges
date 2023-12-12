0ac72aa13edfa5a88926c282a00cd1fb6d04b87c
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-12 12:27:54 +0100

    calculate and display the pizza gain

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -9,15 +9,27 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
-// define the function calculatePizzaGain here
+function calculatePizzaGain(diameter1, diameter2) {
+  let area1 = (Math.PI * (diameter1 / 2)) ^ 2;
+  let area2 = (Math.PI * (diameter2 / 2)) ^ 2;
+
+  let pizzaGain = ((area2 - area1) / area1) * 100;
+  output.textContent = Math.round(pizzaGain);
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
