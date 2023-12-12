baaa395bcd93edf934e3d2e9357f1b51634d5847
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-12 14:02:47 +0100

    part 2

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -13,6 +13,8 @@ pizzaInput1.addEventListener("input", () => {
   const pizzaSize2 = pizzaInput2.value;
 
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+
+  updatePizzaDisplay(pizza1, pizzaSize1);
 });
 
 pizzaInput2.addEventListener("input", () => {
@@ -20,6 +22,8 @@ pizzaInput2.addEventListener("input", () => {
   const pizzaSize2 = pizzaInput2.value;
 
   calculatePizzaGain(pizzaSize1, pizzaSize2);
+
+  updatePizzaDisplay(pizza2, pizzaSize2);
 });
 
 // Task 1
@@ -32,7 +36,10 @@ function calculatePizzaGain(diameter1, diameter2) {
 }
 
 // Task 2
-// define the function updatePizzaDisplay here
+function updatePizzaDisplay(pizzaElement, newSize) {
+  let newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = newDisplaySize + "px";
+}
 
 // Task 3
 // define the function updateOutputColor here
