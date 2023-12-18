4745ece96ce39bf0b7f077eef1f8b11adce18e1d
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-18 05:16:00 +0100

    Part 2: nav items

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -40,6 +40,23 @@ for (const key in languages) {
 }
 
 // Part 2: Creating a Navigation Bar
+// Before we create a navigation bar, let's have a look what's already there in `./js/index.js` :
+
+// - There is a `nav` object with the keys "home", "about", and "contact".
+// - Each value is an object containing an "href" and "text" key and a string value.
+// - A `nav` and `ul` element is created and appended to the `main` element.
+
+// Your task is to fill this list with list items and anchor tags; as content,
+// use the `nav` object which you can access with a `for...in` loop.
+
+// Use the following hints as a guideline:
+
+// - Write a `for...in` loop for the `nav` object.
+// - In each iteration,
+//   - create a list item and an anchor tag
+//   - set the anchor tag's `href` and `textContent` properties to the relevant values of the `nav` object;
+// you want to use bracket notation with your iterator variable
+//   - append your created items accordingly.
 
 const nav = {
   home: { href: "/home", text: "Home" },
@@ -52,6 +69,11 @@ const ul = document.createElement("ul");
 main.append(navElement);
 navElement.append(ul);
 
-// --v-- write/change code here --v--
-
-// --^-- write/change code here --^--
+for (const key in nav) {
+  const li = document.createElement("li");
+  ul.append(li);
+  const a = document.createElement("a");
+  li.append(a);
+  a.href = nav[key].href; // li.HREF = nav[key][href];
+  a.textContent = nav[key].text;
+}
