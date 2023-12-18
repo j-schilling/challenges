0c151cff2902253e26d656fffa4fd7050843e957
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-18 04:46:14 +0100

    Part 1: Creating a select input

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -3,6 +3,19 @@ console.clear();
 const main = document.querySelector('[data-js="main"]');
 
 // Part 1: Creating a Select Input
+// ### Part 1: Creating a Select Input
+
+// Looking at the `./js/index.js` , there is a `languages` object with some key-value pairs.
+// Additionally, a `select` element with the name "languages" is created and appended to the main element.
+
+// Iterate through the `languages` object using `for...in` and add the `option` elements for this dropdown menu.
+
+// The following hints may guide you:
+
+// - Create one `option` element for each entry in `languages`.
+// - For each option, set its `value` and `textContent` to the value of each entry in `languages`.
+//You will need bracket notation, e.g. `languages[myIteratorName]`.
+// - Don't forget to append each option!
 
 const languages = {
   DE: "German",
@@ -20,9 +33,11 @@ const select = document.createElement("select");
 select.name = "languages";
 main.append(select);
 
-// --v-- write/change code here --v--
-
-// --^-- write/change code here --^--
+for (const key in languages) {
+  const option = document.createElement("option");
+  select.append(option);
+  option.textContent = languages[key];
+}
 
 // Part 2: Creating a Navigation Bar
 
