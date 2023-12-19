19153a39690038646ad2563ba16223aa0aaed79c
Janosch Schilling <janosch.schilling@gmail.com>
2023-12-19 16:20:47 +0100

    js callback function callback functions with multiple arguments

--- js-callback-function/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ js-callback-function/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -3,5 +3,12 @@ console.clear();
 function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe", "admin");
 }
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}!`);
+}
 
-// The exercise starts here!
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}!`);
+});
