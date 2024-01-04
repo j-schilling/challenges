8bfe68a6e8d606f0212d67a046bd4698ca3cc5eb
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-04 16:38:31 +0100

    launching the rocket

--- js-structure/js-structure_spacerocket/js/launchSequence.js
+++ js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,21 @@
 // Implement the launch sequence function here and export it as the default export.
+import { NFSAT } from "../js/payload/satellites.js";
+import { FISHSAT } from "../js/payload/satellites.js";
+import { loadPayload } from "../js/core/load.js";
+import { fuel } from "../js/core/fuel.js";
+import { countdown } from "../js/core/countdown.js";
+import { liftoff } from "../js/core/liftoff.js";
+import { deployPayload } from "../js/core/deploy.js";
+
+export default function launchSequence() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  liftoff();
+  deployPayload();
+}
