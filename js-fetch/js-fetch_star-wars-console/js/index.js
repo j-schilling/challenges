249258274222e77195fd15b18de7a91c7d06efe5
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-08 16:00:11 +0100

    JS Fetch: Star Wars API - Console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,21 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    console.log("response: ", response);
+    const data = await response.json();
+    console.log("data: ", data);
+    const results = data.results;
+    console.log("results: ", results);
+
+    console.log(results[5].name);
+    console.log(results[7]);
+    console.log(results[7].eye_color);
+  } catch {
+    console.log("error");
+  }
+}
 
 fetchData();
