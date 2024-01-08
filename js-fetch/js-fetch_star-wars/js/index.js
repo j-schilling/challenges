f856a4ddb629563562eaf1f0387fd1c7ec12894d
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-08 16:24:39 +0100

    Task 1

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,9 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  console.log("response: ", response);
+  const data = await response.json();
+  console.log("data: ", data);
 }
