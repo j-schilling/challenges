ab368af85bb165d422d5d76039e0c334e89268d5
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-15 11:03:22 +0100

    React Basics: Hello World Article

--- react-basics/react-basics_hello-world-article/src/App.js
+++ react-basics/react-basics_hello-world-article/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello Worldio</h1>
+      <p>This is Janoschs article about the World</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
