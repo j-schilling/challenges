65c7805d596a1d8a93aa48d842826229085ec52f
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-16 10:45:07 +0100

    React Nesting: Buttons and Children

--- react-nesting/react-nesting_buttons-and-children/src/App.js
+++ react-nesting/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Button no uno</Button>
+      <Button>Second Button</Button>
+      <Button>Thirdiesh Button</Button>
+      <Button>Der Vierte</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
