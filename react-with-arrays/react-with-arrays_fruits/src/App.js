a93be4621d79ae75a6de692c20e6aeafb5da40fc
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-17 15:21:21 +0100

    react with arrays: Fruits

--- react-with-arrays/react-with-arrays_fruits/src/App.js
+++ react-with-arrays/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,38 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: "0001",
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: "0002",
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: "0003",
+      name: "🍏 Apple",
+      color: "green",
+    },
+    {
+      id: "0004",
+      name: "🍒 Cherry",
+      color: "red",
+    },
+    {
+      id: "0005",
+      name: "🫐 Blueberry",
+      color: "blue",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits &&
+        fruits.map((fruit) => <Card name={fruit.name} key={fruit.id} />)}
     </div>
   );
 }
