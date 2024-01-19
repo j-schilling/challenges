c8242c82d88be872df7255cf3065593faac120a4
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-19 12:17:06 +0100

    React State 3: Tags Tags Tags

--- react-state-3/react-state-3_tags/src/App.js
+++ react-state-3/react-state-3_tags/src/App.js
@@ -6,10 +6,23 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  const handleDeleteTag = (tagToDelete) => {
+    const filtered = tags.filter((tag) => tag !== tagToDelete);
+    console.log("filtered: ", filtered);
+    setTags(filtered);
+  };
+  // const handleAddTag = (newTag) => {
+  //   setTags(...tags, newTag);
+  //   console.log(handleAddTag);}
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
