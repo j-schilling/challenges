0f4ac04010df853ada375a11363a4c7cde91b103
Janosch Schilling <janosch.schilling@gmail.com>
2024-01-29 15:51:39 +0100

    solve

--- react-immutable-state/react-immutable-state_form/components/Form/index.js
+++ react-immutable-state/react-immutable-state_form/components/Form/index.js
@@ -10,11 +10,29 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    setMountain((prevMountain) => {
+      return { ...prevMountain, name: event.target.value };
+    });
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    setMountain((prevMountain) => {
+      return {
+        ...prevMountain,
+        values: { ...prevMountain.values, altitude: event.target.value },
+      };
+    });
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    setMountain((prevMountain) => {
+      return {
+        ...prevMountain,
+        values: { ...prevMountain.values, mountainRange: event.target.value },
+      };
+    });
+  }
 
   return (
     <StyledForm>
